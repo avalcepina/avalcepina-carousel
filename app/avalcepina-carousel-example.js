380d61e1cf1748ff32238248d9aaa9343c702dfb
Avalcepina <valcepina@gmail.com>
2015-05-11 17:24:03 +0200

    Removed debug stuff

--- app/avalcepina-carousel-example.js
+++ app/avalcepina-carousel-example.js
@@ -1,33 +1,28 @@
 (function(angular) {
-  'use strict';
+	'use strict';
 
-  angular.module('myApp', ['avalcepina-carousel'])
-    .controller('myCtrl', function($scope) {
+	angular.module('myApp', ['avalcepina-carousel'])
+		.controller('myCtrl', function($scope) {
 
-      $scope.foo = 123;
-      $scope.bar = 3456;
+			$scope.slides = [{
+				urlLarge: 'http://lorempixel.com/800/320/sports/5',
+				urlSmall: 'http://lorempixel.com/400/160/sports/5'
+			}, {
+				urlLarge: 'http://lorempixel.com/800/320/sports/3',
+				urlSmall: 'http://lorempixel.com/400/160/sports/3'
+			}, {
+				urlLarge: 'http://lorempixel.com/800/320/sports/7',
+				urlSmall: 'http://lorempixel.com/400/160/sports/7'
+			}, {
+				urlLarge: 'http://lorempixel.com/800/320/sports/9',
+				urlSmall: 'http://lorempixel.com/400/160/sports/9'
+			}, {
+				urlLarge: 'http://lorempixel.com/800/320/sports/1',
+				urlSmall: 'http://lorempixel.com/400/160/sports/1'
+			}, {
+				urlLarge: 'http://lorempixel.com/800/320/sports/4',
+				urlSmall: 'http://lorempixel.com/400/160/sports/4'
+			}];
 
-      $scope.slides = [{
-        urlLarge: 'http://lorempixel.com/800/320/sports/5',
-        urlSmall: 'http://lorempixel.com/400/160/sports/5'
-      }, {
-        urlLarge: 'http://lorempixel.com/800/320/sports/3',
-        urlSmall: 'http://lorempixel.com/400/160/sports/3'
-      }, {
-        urlLarge: 'http://lorempixel.com/800/320/sports/7',
-        urlSmall: 'http://lorempixel.com/400/160/sports/7'
-      }, {
-        urlLarge: 'http://lorempixel.com/800/320/sports/9',
-        urlSmall: 'http://lorempixel.com/400/160/sports/9'
-      }, {
-        urlLarge: 'http://lorempixel.com/800/320/sports/1',
-        urlSmall: 'http://lorempixel.com/400/160/sports/1'
-      }, {
-        urlLarge: 'http://lorempixel.com/800/320/sports/4',
-        urlSmall: 'http://lorempixel.com/400/160/sports/4'
-      }];
-
-      // window.console.log($scope);
-
-    });
+		});
 })(angular);
